c156771282f20960bf8fe6c920e0e02b90246da6
Alonso Ortega <alonsojortega@gmail.com>
2017-08-24 00:29:07 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,59 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  var newArr = [];
+  for (var i = 0; i<array.length; i++){
+    newArr[i] = array[i];
+  }
+  newArr.unshift(element);
+  return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  var newArr = [];
+  for (var i = 0; i<array.length; i++){
+    newArr[i] = array[i];
+  }
+  newArr.push(element);
+  return newArr;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  array = array.slice(array.length-1);
+  return array;
+}
+
+console.log(addElementToBeginningOfArray(chocolateBars, 'candy'));
+console.log(chocolateBars);
+
+
+
+
+/*var cities = ["New York", "San Francisco"]
+cities.unshift("Philadelphia")
+console.log(cities)*/
